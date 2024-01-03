feec1d08acf2b6796bcd6a9c9ee6475fc7f2a82d
Ronaldo Schmidt <117224863+schmidtronaldo@users.noreply.github.com>
2024-01-02 22:26:28 -0300

    corrigindo erros de upload

---
+++ src/scripts/README.md
@@ -0,0 +1,7 @@
+Music by <a href="https://pixabay.com/users/brentindavis-27911591/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=146379">Brentin Davis</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=146379">Pixabay</a>
+
+Sound Effect from <a href="https://pixabay.com/sound-effects/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6749">Pixabay</a>
+
+Sound Effect by <a href="https://pixabay.com/pt/users/u_31vnwfmzt6-31480456/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=126627">Shagor Miah</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=126627">Pixabay</a>
+
+Sound Effect from <a href="https://pixabay.com/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=6185">Pixabay</a>
---
+++ src/scripts/engine.js
@@ -0,0 +1,80 @@
+let cartasAbertas
+//musica de fundo
+let bgMusic = document.querySelector('audio');
+bgMusic.volume = 0.3;
+bgMusic.play();
+
+//efeitos sonoros
+let corvo = new Audio('./src/audio/corvo.mp3');
+let error = new Audio('./src/audio/error.mp3');
+let success = new Audio('./src/audio/success.mp3');
+success.volume = 0.3;
+
+//cartas com personagens
+const atores =[
+  'cat',
+  'giga',
+  'gobo',
+  'nano',
+  'penguin',
+  'pico',
+  'pony',
+  'tera',
+  'unicorn'
+];
+//duplica lista de atores
+const embaralharAtores = [... atores , ...atores];
+//embaralha ordem dos personagens
+const cartas = embaralharAtores.sort(()=> Math.random() - 0.5);
+//salva cartas clicadas
+let openCards = [];
+//adiciona as cartas dinamicamente no html
+for (let i = 0; i < cartas.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.style.backgroundImage = `url('./src/img/${cartas[i]}.svg')`;
+  box.onclick = handleClick;
+  document.querySelector(".game").appendChild(box);
+}
+
+//verifica quantidade de cartas abertas
+function handleClick() {
+  
+  
+  
+  if (openCards.length < 2 && this.classList.length<2) {
+    this.classList.add("boxOpen");
+    openCards.push(this);
+    corvo.play();
+    
+    
+  }
+
+  if (openCards.length == 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
+
+//verifica se cartas tem mesmo valor
+function checkMatch() {
+  
+  if (openCards[0].outerHTML === openCards[1].outerHTML) {
+    success.play();
+    openCards[0].classList.add("boxMatch");
+    openCards[1].classList.add("boxMatch");
+  } else {
+    error.play();
+    openCards[0].classList.remove("boxOpen");
+    openCards[1].classList.remove("boxOpen");
+    
+  }
+  openCards = [];
+ 
+ // verifica condiçao de vitoria
+  if (document.querySelectorAll(".boxMatch").length === cartas.length) {
+    let textoVitoria = document.querySelector('.titulo');
+    textoVitoria.innerHTML = "Você venceu! Parabéns!";
+  }
+}
+
+
